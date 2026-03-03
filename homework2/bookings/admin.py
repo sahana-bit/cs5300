13e69baaaf0b587dd60108599af3f532fb84c22f
Sahana Vinayak <sahanavinayak@gmail.com>
2026-03-03 08:58:30 -0700

    Modified Seat Numbers

--- homework2/bookings/admin.py
+++ homework2/bookings/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Movie, Seat, Booking
+from .models import Movie,  Seat, Booking
 
 admin.site.register(Movie)
 admin.site.register(Seat)
--- homework2/bookings/models.py
+++ homework2/bookings/models.py
@@ -27,3 +27,4 @@ class Booking(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.movie.title}"
+    
