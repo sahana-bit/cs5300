68d1c4b45cbe5af99a87c106466dbe8c81b5fa74
Sahana Vinayak <sahanavinayak@gmail.com>
2026-03-03 00:42:10 -0700

    Implemented MVT architecture

--- homework2/bookings/models.py
+++ homework2/bookings/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    release_date = models.DateField()
+    duration = models.IntegerField()  # in minutes
+
+    def __str__(self):
+        return self.title
+
+
+class Seat(models.Model):
+    seat_number = models.CharField(max_length=10)
+    is_booked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.seat_number
+
+
+class Booking(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    booking_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.movie.title}"
--- homework2/bookings/views.py
+++ homework2/bookings/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Movie, Seat, Booking
+from .serializers import MovieSerializer, SeatSerializer, BookingSerializer
 
-# Create your views here.
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class SeatViewSet(viewsets.ModelViewSet):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
